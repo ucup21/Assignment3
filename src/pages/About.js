fc3c6e0bdd7c1d5015de122f0481b85ea5126ea3
yusup supriyadi <yusupubl14@gmail.com>
2022-08-27 00:28:07 +0700

    change style

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 function About() {
     return (
-        <div>
+        <div className='about'>
             <h1><b>Yusup Supriyadi</b></h1>
             <address style={{color:'blue'}}>yusupubl14@gmail.com</address>
             <h2 style={{color:'red'}}>RCTN064ONL026</h2>
